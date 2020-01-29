2d872527a3fc61fb1600b76d1c9702d7d6399f50
Yamini-R-99 <60396625+Yamini-R-99@users.noreply.github.com>
2020-01-29 15:52:54 +0530

    Create lcm.py

---
+++ lcm.py
@@ -0,0 +1,9 @@
+def gcd(num1,num2):
+    if num1%num2 == 0:
+         return num2
+    else:
+         return gcd(num2,num1%num2)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+lcm = (num1*num2)//gcd(num1,num2)
+print("The L.C.M. of", num1,"and", num2,"is", lcm)
